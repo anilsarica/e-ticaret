23709543ed87835f4effd8e988df67ad0ae323b7
anilsarica <anilsarica1907@gmail.com>
2023-04-15 18:38:20 +0300

    14.4

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,18 @@
 //// tüm linklerin olduğu başlık kısmı
 import React, { useState } from 'react'
 import styles from "./Header.module.scss"
-import {Link} from "react-router-dom"
-import { FaShoppingCart } from "react-icons/fa"
+import {Link,NavLink, useNavigate} from "react-router-dom"
+import { FaShoppingCart, FaTimes } from "react-icons/fa"
 import { HiOutlineMenuAlt3 } from "react-icons/hi"
+import { signOut } from "firebase/auth"
+import { auth } from "../../firebase/config"
+import { toast } from "react-toastify"
 
 const Header = () => {
 
   const [showMenu,setShowMenu] =useState(false)
+
+  const navigate = useNavigate()
   
   const toggleMenu = () => {
     setShowMenu(!showMenu)
@@ -16,6 +21,15 @@ const Header = () => {
   const hideMenu = () => {
     setShowMenu(false)
   } 
+
+  const logoutUser = () => {
+    signOut(auth).then(() => {
+      toast.success("Logout Successfully..")
+      navigate("/")
+    }).catch((error) => {
+      toast.error(error.message)
+    });
+  }
 
   const logo = (
     <div className={styles.logo}>
@@ -36,23 +50,30 @@ const Header = () => {
     </span>
   ) 
 
+  const activeLink = (({isActive}) => (isActive ? `${styles.active}` : ""))
+
   return (
     <header>
       <div className={styles.header}>{logo}
         <nav className={ showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
           <div className={ showMenu ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}` : `${styles["nav-wrapper"]}`} onClick={hideMenu}></div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>
+              {logo}
+              <FaTimes size={22} color="#fff" onClick={hideMenu}/>
             </li>
             <li>
-              <Link to="/contact">Contact Us</Link>
+              <NavLink to="/" className={activeLink}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className={activeLink}>Contact Us</NavLink>
             </li>
           </ul>
-          <div className={styles["header-right"]}>
+          <div className={styles["header-right"]} onClick={hideMenu}>
             <span className={styles.links}>
-              <Link to="/login">Login</Link>
-              <Link to="/order-history">My Orders</Link>
+              <NavLink to="/login" className={activeLink}>Login</NavLink>
+              <NavLink to="/order-history" className={activeLink}>My Orders</NavLink>
+              <NavLink to="/" onClick={logoutUser}>Logout</NavLink>
             </span>
             {cart}
           </div>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,27 @@
 //// burada google firebase ile etkileşime buradan girilecek.
+//firebase ile etkileşime geçildi
+
+import { initializeApp } from "firebase/app";
+//burası bizim için authentication işlemileri için(kullanıcı kaydı)
+import { getAuth } from "firebase/auth" 
+//firestore database erişimi için
+import {getFirestore} from "firebase/firestore"
+//storage erişimi için (resim kayıt yeri)
+import { getStorage } from "firebase/storage"
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCVb5xTnD3G9DvaRJpb1D8zMxwCF1jzkFg",
+  authDomain: "e-ticaret-class-8de42.firebaseapp.com",
+  projectId: "e-ticaret-class-8de42",
+  storageBucket: "e-ticaret-class-8de42.appspot.com",
+  messagingSenderId: "962142490080",
+  appId: "1:962142490080:web:a0744a794b7ae7194baf85"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+
+export default app
